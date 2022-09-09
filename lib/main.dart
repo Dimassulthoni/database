7ba46f0c081e7f97171883f72eb1a79f996b2a3e
Dimassulthoni <dimas.sulthoni12@gmail.com>
2022-09-09 09:00:31 +0700

    main mantap

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:database/list_kontak.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
+import 'model/kontak.dart';
+import 'form_kontak.dart';
+import 'database/db_helper.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: listKontakPage(),
+      home: listKontakPage(),
     );
   }
 }
